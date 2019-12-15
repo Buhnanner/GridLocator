5fee552aa99ef88e0b270c8795228eea7ec8f907
Kipland Melton <kipmelton@yahoo.com>
2019-12-15 08:16:08 -0600

    Add files via upload

---
+++ Master.py
@@ -0,0 +1,19 @@
+"""
+A simple program that can be used as a plugin for games to find your place on a virtual grid
+Kipland Melton
+"""
+
+
+def Locator():
+
+
+    x = 0
+    y = 0
+
+
+    if x == 0 and y == 0:
+        player_loc = 'Spawn'
+
+
+
+Locator()
